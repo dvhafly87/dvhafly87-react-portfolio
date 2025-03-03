510f1222179418b7eddec3d8a6fedef75b7e760f
dvhafly87 <dvhafly87@gmail.com>
2025-03-03 22:59:05 +0900

    저장소 추가

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/<dvhafly87-react-portfolio>/', // 저장소 이름을 넣어주세요. 예를 들어 '/my-react-app/'
+});
